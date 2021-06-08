305e9255fb32d25a7c3c286cbc2bf051763afafc
Bob Torgerson <rltorgerson@alaska.edu>
2021-06-07 17:24:46 -0800

    Updates Geoserver Snow Cover store. Can be run as cronjob as root user.

--- update_snow_cover.sh
+++ update_snow_cover.sh
@@ -16,8 +16,9 @@ fi
 
 fname="NIC.IMS_v3_${ymd}_1km.tif"
 tname="snow_cover_3338.tif"
-wget -nc -P /tmp --content-disposition "https://usicecenter.gov/File/DownloadProduct?products=%2Fims%2Fims_v3%2Fimstif%2F1km%2F${year}&fName=${fname}.gz"
-gunzip -f /tmp/$fname
+wget -nc --content-disposition -O "/tmp/$fname.gz" "https://usicecenter.gov/File/DownloadProduct?products=%2Fims%2Fims_v3%2Fimstif%2F1km%2F${year}&fName=${fname}.gz"
+gunzip -f /tmp/$fname.gz
 gdal_translate -projwin -175.0 50.0 -80.0 55.0 -projwin_srs EPSG:4326 /tmp/$fname /tmp/snow_cover_crop.tif
 gdalwarp -overwrite -t_srs EPSG:3338 /tmp/snow_cover_crop.tif /tmp/snow_cover_warp.tif
 gdal_translate -projwin 173.2 77.0 -118.0 46.0 -projwin_srs EPSG:4326 /tmp/snow_cover_warp.tif /tmp/$tname
+mv /tmp/$fname /usr/share/geoserver/data_dir/data/alaska_wildfires/ 
