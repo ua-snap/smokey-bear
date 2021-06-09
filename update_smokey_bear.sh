7435d7914349eaa5769b1869f44ba5f10cfe3917
Bob Torgerson <rltorgerson@alaska.edu>
2021-06-08 16:26:35 -0800

    Creates both the snow cover layer and smokey bear layer, puts them into place in Geoserver, and initiates a complete reseed of the GeoWebCache.

---
+++ update_smokey_bear.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# Downloads data from upstream source,
+# Updates layer in GeoServer,
+# Rebuilds TileCache layer.
+
+set -x
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	ymd=$( date -d "yesterday" '+%Y%m%d' )
+	year=$( date -d "yesterday" '+%Y' )
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    ymd=$( date -v-1d +%Y%m%d )
+    year=$( date -v-1d +%Y )
+else
+	echo "OS unknown?"
+	exit 1
+fi
+
+akfile="${ymd}_spruce"
+akdownload="https://akff.mesowest.org/static/grids/tiff/${akfile}.tiff"
+wget -nc -P /tmp ${akdownload}
+gdalwarp -t_srs EPSG:3338 /tmp/${akfile}.tiff /tmp/spruceadj_3338.tif
+
+# Reseeds the tile cache
+curl -v -u admin:${admin_pass} -XPOST -H "Content-type: text/xml" -d '<seedRequest><name>alaska_wildfires:spruceadj_3338</name><srs><number>3338</number></srs><zoomStart>0</zoomStart><zoomStop>7</zoomStop><format>image/png</format><type>reseed</type><threadCount>4</threadCount></seedRequest>'  "http://gs.mapventure.org:8080/geoserver/gwc/rest/seed/alaska_wildfires:spruceadj_3338.xml"
