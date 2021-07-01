96b55f3ef4e5f05b59c71065c32a591d09740185
Bruce Crevensten <bruce.crevensten@gmail.com>
2021-07-01 13:56:23 -0700

    Remove files not used in current production environment

--- spruceadj.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l
-#Needs to be run with conda activate python36_geo for gdal
-# 2020/06 Chris Waigl cwaigl@alaska.edu
-
-bash ./spruceadjAK.sh
-bash ./spruceadjCA.sh
-bash ./spruceadjmerge.sh
