ef5291ce95c633c68d36b0e52e2f0fb6b7e00c76
Bob Torgerson <rltorgerson@alaska.edu>
2024-02-23 13:01:54 -0900

    Updates snow cover script to work with Prefect.

--- update_snow_cover.sh
+++ update_snow_cover.sh
@@ -8,6 +8,7 @@ set -x
 source ~/.adminpass
 
 # Activates the smokeybear Conda environment with GDAL installed.
+source /opt/miniconda3/bin/activate
 conda activate smokeybear
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -21,12 +22,15 @@ else
 	exit 1
 fi
 
+script_dir=$(dirname "$(readlink -f "$0")")
+GEOSERVER_HOME=/usr/share/geoserver
+
 fname="NIC.IMS_v3_${ymd}_1km.tif"
 tname="snow_cover_3338.tif"
 wget -nc --content-disposition -O "/tmp/$fname.gz" "https://usicecenter.gov/File/DownloadProduct?products=%2Fims%2Fims_v3%2Fimstif%2F1km%2F${year}&fName=${fname}.gz"
 gunzip -f /tmp/$fname.gz
 gdal_translate -projwin -175.0 50.0 -80.0 55.0 -projwin_srs EPSG:4326 /tmp/$fname /tmp/snow_cover_crop.tif
-akcoast=./shapefiles/Alaska_Coast_Simplified_POLYGON.shp
+akcoast="$script_dir/shapefiles/Alaska_Coast_Simplified_POLYGON.shp"
 gdalwarp -crop_to_cutline -cutline ${akcoast} -overwrite -t_srs EPSG:3338 /tmp/snow_cover_crop.tif /tmp/snow_cover_warp.tif
 gdal_translate -projwin 173.2 77.0 -118.0 46.0 -projwin_srs EPSG:4326 /tmp/snow_cover_warp.tif /tmp/$tname
 mv /tmp/$tname $GEOSERVER_HOME/data_dir/data/alaska_wildfires/
